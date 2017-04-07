ac5852346ecd9b1cd780c854488cd2097a3ccbd3
KarateJB <ppippenkimo@gmail.con>
2017-04-07 18:14:17 +0800

    Refactoring.

--- src/src/app/class/FirebaseConfig.ts
+++ src/src/app/class/FirebaseConfig.ts
@@ -4,11 +4,11 @@ export module FirebaseConfig {
 
     export function Get() {
         config = {
-            apiKey: "AIzaSyCZzDE0KS0jj4WIW424-92w86lerFaLiKU",
-            authDomain: "fir-e1a22.firebaseapp.com",
-            databaseURL: "https://fir-e1a22.firebaseio.com",
-            storageBucket: "fir-e1a22.appspot.com",
-            messagingSenderId: "499014936125"
+            apiKey: "",
+            authDomain: "",
+            databaseURL: "",
+            storageBucket: "",
+            messagingSenderId: ""
         };
 
         return config;
